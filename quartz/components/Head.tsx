6d936af1f69511a1ea7d45cef7d0b995a0f854aa
shrimptqm <15355495103@163.com>
2024-05-28 22:04:32 +0800

    Quartz sync: May 28, 2024, 10:04 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -20,48 +20,15 @@ export default (() => {
 
     return (
       <head>
-        /*<script
-          src="https://notesss.yxy.ninja/script.js"
-          type="text/javascript">
-        </script>
-        <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.DD_RUM && window.DD_RUM.init({
-              clientToken: 'pubf8e87c77bc40109caafef497cae669d9',
-              applicationId: '1790642d-89c4-45b7-8ba8-b2d58a5212b2',
-              proxy: 'https://notesss.yxy.ninja',
-              site: 'datadoghq.com',
-              service: 'notes.yxy.ninja',
-              env: 'prod',
-              // Specify a version number to identify the deployed version of your application in Datadog 
-              // version: '1.0.0', 
-              sessionSampleRate: 100,
-              sessionReplaySampleRate: 100,
-              trackUserInteractions: true,
-              trackResources: true,
-              trackLongTasks: true,
-              defaultPrivacyLevel: 'allow',
-              usePartitionedCrossSiteSessionCookie: true,
-            });
-          `,
-        }}
-        /> 
-        */
-
-        /*
-        <script async src="https://rum.cronitor.io/script.js"></script>
-        <script
-        dangerouslySetInnerHTML={{
-            __html: `
-              window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
-              cronitor('config', { clientKey: 'fe62c9349bb06b60dd67e689bebb2838' });`
-          }}
-        >
-        </script>
-         */
-        
-        
+        <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c7a22fad6f8728edcc5e34550ba90fc7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>    
 
 
         <title>{title}</title>
